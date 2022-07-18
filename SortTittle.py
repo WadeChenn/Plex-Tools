a26b76b1d5fe3302c0154ecb2a4ac7e48d2b54b7
WadeChenn <821204616@qq.com>
2022-07-18 11:50:49 +0800

    Update SortTittle.py

--- SortTittle.py
+++ SortTittle.py
@@ -2,12 +2,12 @@
 # 或联系 yerwer@foxmail.com
 # 库名参数-l (库名或 all ) -n (库编号) -c (是否覆盖 1 or 0) -log(1 or 0 是否打开进度条log)
 #########################参数初始化(使用配置文件请修改此处)############################
-USE_INIT=False                               #使用当前配置请设为True,否则将尝试从外部获取参数
-PLEX_TOKEN="zZzxxxxxxxJssiw2zcy"           #Plextoken获取,具体方法请自行查找 "zZzxxxxxxxJssiw2zcy" *必须设置
+USE_INIT=True                               #使用当前配置请设为True,否则将尝试从外部获取参数
+PLEX_TOKEN="zZzxxxxxxxJssiw2zcy"            #Plextoken获取,具体方法请自行查找 "zZzxxxxxxxJssiw2zcy" *必须设置
 PLEX_URL="http://plex.xxx.cn:32400"         #Plexurl "http://plex.xxx.cn:32400" *必须设置
 RECOVER=False                               #是否覆盖已有的拼音排序
 LIB_NAME=''                                 #要排序的库名(存在时库编号不生效)
-LIB_NUMBER=6                                #要排序的库编号(不使用库编号则设为0)
+LIB_NUMBER=0                                #要排序的库编号(不使用库编号则设为0)
 ENABLE_LOG=1                                #是否输出进度条
 ##############################################################
 
