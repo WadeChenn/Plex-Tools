04f660069a6127eefde90aba67081e06d54f1172
BeiMou <821204616@qq.com>
2022-10-10 21:01:05 +0800

    Update qbit_upload_limit.py

--- Qbit_upload_limit/qbit_upload_limit.py
+++ Qbit_upload_limit/qbit_upload_limit.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
         for player in session.players:
             address=player.address.split('.')[0]
             if address not in ineraddress and player.state!='paused':
-                for s in session.sessions:
+                for s in session.session:
                     bandwidth=bandwidth+s.bandwidth/1024/8
                 needlimit=True
     SPEEDLIMIT=(NET_BANDWIDTH-bandwidth)*1024*1024
