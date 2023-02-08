7871de8fbf1ba427755ed3595dd09abe9e5d5b49
cyw <cyw@turesightai.com>
2023-02-08 11:14:46 +0800

    [update] 添加log

--- plugins/plexsortout/event.py
+++ plugins/plexsortout/event.py
@@ -36,11 +36,12 @@ def on_event(ctx: PluginContext, event_type: str, data: Dict):
     触发绑定的事件后调用此函数
     函数接收参数固定。第一个为插件上下文信息，第二个事件类型，第三个事件携带的数据
     """
+    _LOGGER.info('接收到 PlexActivityEvent 事件')
     if data.get('Activity') == 'Added' and data.get('Added'):
         plexst.process()
     # plexst.send_by_event(event_type, data)
 @plugin.on_event(
-    bind_event=['EventType.DownloadCompleted'], order=1)
+    bind_event=[EventType.DownloadCompleted], order=1)
 def on_event(ctx: PluginContext, event_type: str, data: Dict):
     """
     触发绑定的事件后调用此函数
